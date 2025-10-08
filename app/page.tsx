b56927316a3902dae36b49cdc445c15a6401cc99
tallasamuhammad-thefuturebender <tallasamuhammad@gmail.com>
2025-10-08 08:50:46 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import TrustBadges from '../components/TrustBadges';
 import HeroSection from '../components/HeroSection';
 import FilterBar from '../components/FilterBar';
+import ProductGrid from '../components/ProductGrid';
 
 export default function Home() {
   return (
-    <main>
+    <main className="min-h-screen bg-white py-8">
       <TrustBadges />
       <HeroSection />
       <FilterBar />
+      <ProductGrid />
     </main>
   )
 }
