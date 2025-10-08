b5f4890c2a0d4d421b78c6d8ca21409d12ef5ffe
tallasamuhammad-thefuturebender <tallasamuhammad@gmail.com>
2025-10-08 08:43:30 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import TrustBadges from '../components/TrustBadges';
 import HeroSection from '../components/HeroSection';
+import FilterBar from '../components/FilterBar';
 
 export default function Home() {
   return (
     <main>
       <TrustBadges />
       <HeroSection />
+      <FilterBar />
     </main>
   )
 }
