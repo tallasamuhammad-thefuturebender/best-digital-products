c669e1d6a14d72369b54e80dedf75b92c99bfaee
tallasamuhammad-thefuturebender <tallasamuhammad@gmail.com>
2025-10-08 08:30:52 +0800

    Create HeroSection.tsx

---
+++ components/HeroSection.tsx
@@ -0,0 +1,8 @@
+export default function HeroSection() {
+  return (
+    <div>
+      <h1>Stop Wasting Money on SEO Tools That Don't Work</h1>
+      <p>We tested 25+ SEO platforms to find the 3 that actually deliver results</p>
+    </div>
+  )
+}
