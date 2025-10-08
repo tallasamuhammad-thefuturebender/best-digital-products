94524080fad090cab0a322c9aa9c19eebe3a3071
tallasamuhammad-thefuturebender <tallasamuhammad@gmail.com>
2025-10-08 08:17:38 +0800

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Coming Soon - Best Digital Products</h1>
+      <p>Honest reviews of digital tools and software</p>
+    </main>
+  )
+}
